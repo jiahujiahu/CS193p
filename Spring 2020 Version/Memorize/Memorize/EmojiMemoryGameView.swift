c71f6fa2425a1e6dff328aeb7a0e4d48a7c448ae
Jia Hu <Jia.Hu@trader.ca>
2022-05-26 13:09:52 -0400

    l3

--- Spring 2020 Version/Memorize/Memorize/EmojiMemoryGameView.swift
+++ Spring 2020 Version/Memorize/Memorize/EmojiMemoryGameView.swift
@@ -13,14 +13,13 @@ struct EmojiMemoryGameView: View {
     var body: some View {
         return HStack {
             ForEach(viewModel.cards) { card in
-                CardView(card: card)
-                    .onTapGesture { viewModel.choose(card: card) }
-                    .aspectRatio(2/3, contentMode: .fit)
+                CardView(card: card).onTapGesture {
+                    viewModel.choose(card: card)
+                }
             }
         }
-            .padding(10)
+            .padding()
             .foregroundColor(Color.orange)
-            .font(Font.largeTitle)
     }
 }
 
@@ -28,15 +27,30 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
         ZStack {
             if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
                 Text(card.content)
             } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
+                RoundedRectangle(cornerRadius: edgeLineWidth).fill()
             }
         }
+        .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    // MARK: - Drawing Constants
+    
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3  
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
     }
 }
 
