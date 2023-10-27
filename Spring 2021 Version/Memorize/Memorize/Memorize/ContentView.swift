c148bbeea0b6b8f5c24df936e9d5dd9a25660b77
Jia Hu <jia.hu0918@gmail.com>
2023-10-27 00:36:19 -0400

    lec 1 watched

--- Spring 2021 Version/Memorize/Memorize/Memorize/ContentView.swift
+++ Spring 2021 Version/Memorize/Memorize/Memorize/ContentView.swift
@@ -9,10 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack { // stacking them towards us
+            RoundedRectangle(cornerRadius: 20.0)
+                .stroke(lineWidth: 3)
+            Text("Hello, world")
+        }
+        .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+
+
+
+
     }
 }
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
